ddadd1dc176ac500a7721d09d24ddfe195810a8b
Billy Yang <mz.yyam@gmail.com>
2023-07-22 19:13:41 +0800

    load

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,21 @@
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/sim-web.omp.json" | Invoke-Expression
+function Run-Step([string] $Description, [ScriptBlock]$script)
+{
+  Write-Host  -NoNewline "Loading " $Description.PadRight(20)
+  & $script
+  Write-Host "`u{2705}" # checkmark emoji
+}
+
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 
-Import-Module PSReadLine
+# oh-my-posh
+Run-Step "oh-my-posh" {
+  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/sim-web.omp.json" | Invoke-Expression
+}
+Run-Step "PSReadline" {
+  Import-Module PSReadLine
+  Set-PSReadLineOption -EditMode Emacs
+}
 
-Set-PSReadLineOption -EditMode Emacs
+
+
