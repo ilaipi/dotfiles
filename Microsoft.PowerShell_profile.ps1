f7f9edf2723602147793effd8bfabf7dacc4c134
Billy Yang <mz.yyam@gmail.com>
2025-04-07 18:05:59 +0800

    feat: 添加对 jabba 的支持以管理 Java 版本
    在 PowerShell 配置文件中添加了对 jabba 的支持，用于管理 Java 版本。通过设置环境变量和定义函数，确保在启动时自动加载默认的 Java 版本。

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -21,4 +21,24 @@ if (Get-Command fnm -ErrorAction SilentlyContinue) {
   fnm env --use-on-cd | Out-String | Invoke-Expression
 }
 
+if (Get-Command jabba -ErrorAction SilentlyContinue) {
+ $env:JABBA_HOME="$((scoop config root_path).Trim())\apps\jabba\current"
+
+  function jabba
+  {
+      $fd3=$([System.IO.Path]::GetTempFileName())
+      $command="& '$env:JABBA_HOME\bin\jabba.exe' $args --fd3 `"$fd3`""
+      & { $env:JABBA_SHELL_INTEGRATION="ON"; Invoke-Expression $command }
+      $fd3content=$(Get-Content $fd3)
+      if ($fd3content) {
+          $expression=$fd3content.replace("export ","`$env:").replace("unset ","Remove-Item env:") -join "`n"
+          if (-not $expression -eq "") { Invoke-Expression $expression }
+      }
+      Remove-Item -Force $fd3
+  }
+  # 初始化 jabba 环境
+  $env:JABBA_SHELL_INTEGRATION="ON"
+  jabba use default
+}
+
 Clear-Host
