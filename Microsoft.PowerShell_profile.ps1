ee4d1a3a7137f8399321d8c0a5bd1fe76ed5e15a
Billy Yang <mz.yyam@gmail.com>
2025-05-07 15:01:21 +0800

    feat: 使用 warp 作为终端，去掉不需要的配置

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,21 @@
-function Run-Step([string] $Description, [ScriptBlock]$script)
-{
-  Write-Host  -NoNewline "Loading " $Description.PadRight(20)
-  & $script
-  Write-Host "`u{2705}" # checkmark emoji
-}
+# function Run-Step([string] $Description, [ScriptBlock]$script)
+# {
+#   Write-Host  -NoNewline "Loading " $Description.PadRight(20)
+#   & $script
+#   Write-Host "`u{2705}" # checkmark emoji
+# }
 
-[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+# [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 
 # oh-my-posh
-Run-Step "oh-my-posh" {
-  oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/sim-web.omp.json" | Invoke-Expression
-}
-Run-Step "PSReadline" {
-  Import-Module PSReadLine
-  Set-PSReadLineOption -EditMode Emacs
-}
+# Run-Step "oh-my-posh" {
+#   oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/sim-web.omp.json" | Invoke-Expression
+# }
+# Run-Step "PSReadline" {
+#   Import-Module PSReadLine
+#   Set-PSReadLineOption -EditMode Emacs
+# }
 
 if (Get-Command fnm -ErrorAction SilentlyContinue) {
   fnm env --use-on-cd | Out-String | Invoke-Expression
@@ -41,4 +41,4 @@ if (Get-Command jabba -ErrorAction SilentlyContinue) {
   jabba use default
 }
 
-Clear-Host
+# Clear-Host
